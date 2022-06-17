90b2d41b2de8ae388c0475fcbd528ac315b815f2
NikhilBanga <coder@nikhilbanga.com>
2022-06-17 18:44:19 +0530

    main

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      // 13 is missing in spacing so we can add it there
+      spacing: {
+        13: '3.25rem',
+      }
+    },
+  },
+  plugins: [],
+}
